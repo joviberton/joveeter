01adfb3e4ed351ac0293ef6cb570166ef2e47134
Jovi Berton <joviberton@gmail.com>
2012-09-12 00:16:57 +0200

    prove su js

--- js/draganddrop.js
+++ js/draganddrop.js
@@ -22,6 +22,19 @@ $(function() {
 $(document).ready(function () {
    $("#listaliste").load("inserimento.php");
    
+           $(".lista").droppable({
+                tolerance: 'touch',
+                over: function() {
+                 $(this).removeClass('lista').addClass('lista-over');
+                },
+                out: function() {
+                 $(this).removeClass('lista-over').addClass('lista');
+                },
+                drop: function(event,ui) {
+//                  $("#listaliste").html("<h1>check</h1>");
+                  $("#listaliste").load("inserimento.php", { 'attori[]': [$(this).attr('id'), ui.draggable.attr('id')] }, function(){} );
+                }
+        });
    
    
 });
