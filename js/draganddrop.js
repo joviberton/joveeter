cede24761a0bb3c8620427fa05455410a77afaa2
Jovi Berton <joviberton@gmail.com>
2012-09-17 18:13:31 +0200

    correzione jquery load

--- js/draganddrop.js
+++ js/draganddrop.js
@@ -1,40 +1,27 @@
-$(function() {
-
+$(document).ready(function () { 
+   $("#listaliste").load("inserimento.php",
+   function(){
+   
         $(".utentelista").draggable({
                 revert: true
         });
-
-        $(".lista").droppable({
-                tolerance: 'touch',
-                over: function() {
-                 $(this).removeClass('lista').addClass('lista-over');
-                },
-                out: function() {
-                 $(this).removeClass('lista-over').addClass('lista');
-                },
-                drop: function(event,ui) {
-//                  $("#listaliste").html("<h1>check</h1>");
-                  $("#listaliste").load("inserimento.php", { 'attori[]': [$(this).attr('id'), ui.draggable.attr('id')] }, function(){} );
-                }
-        });
-});
-
-$(document).ready(function () {
-   $("#listaliste").load("inserimento.php");
    
-           $(".lista").droppable({
-                tolerance: 'touch',
-                over: function() {
-                 $(this).removeClass('lista').addClass('lista-over');
-                },
-                out: function() {
-                 $(this).removeClass('lista-over').addClass('lista');
-                },
-                drop: function(event,ui) {
-//                  $("#listaliste").html("<h1>check</h1>");
-                  $("#listaliste").load("inserimento.php", { 'attori[]': [$(this).attr('id'), ui.draggable.attr('id')] }, function(){} );
-                }
-        });
    
+	   $(".lista").droppable({
+	                tolerance: 'touch',
+	                over: function() {
+	                 $(this).removeClass('lista').addClass('lista-over');
+	                },
+	                out: function() {
+	                 $(this).removeClass('lista-over').addClass('lista');
+	                },
+	                drop: function(event,ui) {
+	                  $("#listaliste").load("inserimento.php", { 'attori[]': [$(this).attr('id'), ui.draggable.attr('id')] }, function(){} );
+	                }
+	        });
+  
+   			} // 
+   
+   );
    
 });
