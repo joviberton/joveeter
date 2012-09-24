d50cb7b1a81cc772b2c88b7ce3f073dbd738c764
Jovi Berton <joviberton@gmail.com>
2012-09-24 21:04:09 +0200

    labeling e lnk

--- js/draganddrop.js
+++ js/draganddrop.js
@@ -1,27 +1,45 @@
+/*
+
+
+      _                     _      _                             
+   _ | |   ___    __ __    (_)    | |_     ___     ___      _ _  
+  | || |  / _ \   \ V /    | |    |  _|   / -_)   / -_)    | '_| 
+  _\__/   \___/   _\_/_   _|_|_   _\__|   \___|   \___|   _|_|_  
+_|"""""|_|"""""|_|"""""|_|"""""|_|"""""|_|"""""|_|"""""|_|"""""| 
+"`-0-0-'"`-0-0-'"`-0-0-'"`-0-0-'"`-0-0-'"`-0-0-'"`-0-0-'"`-0-0-' 
+
+
+
+
+
+
+
+
+
+
+*/
 $(document).ready(function () { 
    $("#listaliste").load("inserimento.php",
-   function(){
-   
-        $(".utentelista").draggable({
-                revert: true
-        });
-   
-   
-	   $(".lista").droppable({
-	                tolerance: 'touch',
-	                over: function() {
-	                 $(this).removeClass('lista').addClass('lista-over');
-	                },
-	                out: function() {
-	                 $(this).removeClass('lista-over').addClass('lista');
-	                },
-	                drop: function(event,ui) {
-	                  $("#listaliste").load("inserimento.php", { 'attori[]': [$(this).attr('id'), ui.draggable.attr('id')] }, function(){} );
-	                }
+	   function(){
+	        $(".utentelista").draggable({
+	                revert: true
 	        });
-  
-   			} // 
-   
-   );
-   
+			$(".lista").droppable({
+		                tolerance: 'touch',
+		                over: function() {
+		                 $(this).removeClass('lista').addClass('lista-over');
+		                },
+		                out: function() {
+		                 $(this).removeClass('lista-over').addClass('lista');
+		                },
+		                drop: function(event,ui) {
+		                  $("#listaliste").load("inserimento.php", { 'attori[]': [$(this).attr('id'), ui.draggable.attr('id')] }, function(){} );
+		                }
+			});
+		} //
+   );   
+});
+
+$(".pag").click(function() {
+	$("#listautenti").load("utenti.php", $(this).attr('id'));
 });
